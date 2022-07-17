a188811e68cef9952a311dc940581c0a96490fc5
Vineeth Menon <vineeth.madhu@gmail.com>
2022-07-16 20:05:08 -0400

    Update variable name

--- test/matrix/test_transpose.py
+++ test/matrix/test_transpose.py
@@ -5,12 +5,12 @@ from src.matrix.transpose import Transpose
 
 class TestTranspose:
     @pytest.mark.parametrize(
-        "input, expected", [([[1, 1, 1], [2, 2, 2], [3, 3, 3]], [[1, 2, 3], [1, 2, 3], [1, 2, 3]]),
+        "input_, expected", [([[1, 1, 1], [2, 2, 2], [3, 3, 3]], [[1, 2, 3], [1, 2, 3], [1, 2, 3]]),
                             ([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]],
                              [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]])
                             ]
     )
-    def test_solution_1_returns_expected_result(self, input, expected):
+    def test_solution_1_returns_expected_result(self, input_, expected):
         matrix_transpose = Transpose()
 
-        assert matrix_transpose.solution_1(input) == expected
+        assert matrix_transpose.solution_1(input_) == expected
