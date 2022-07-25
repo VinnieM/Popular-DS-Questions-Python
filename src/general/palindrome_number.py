ab600ae2058a1b00cb398ad02a5f8e35f1c611c9
Vineeth Menon <vineeth.madhu@gmail.com>
2022-07-25 01:14:21 -0400

    Add comments

--- src/general/palindrome_number.py
+++ src/general/palindrome_number.py
@@ -1,4 +1,8 @@
 class PalindromeNumber:
+    """
+    For more info refer to PEP 238v - https://peps.python.org/pep-0238/
+    """
+
     def solution_1(self, x: int) -> bool:
         if not isinstance(x, int) or x < 0 or (x % 10 == 0 and x != 0):
             return False
@@ -8,5 +12,4 @@ class PalindromeNumber:
             reverted_number = (reverted_number * 10) + (x % 10)
             x //= 10
 
-        reverted_number = int(reverted_number)
         return x == reverted_number or x == reverted_number // 10
