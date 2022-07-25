c18a5cb835f943f0bc7362a37894a806f9b8dc47
Vineeth Menon <vineeth.madhu@gmail.com>
2022-07-25 01:00:53 -0400

    Add Palindrome Number

---
+++ src/general/palindrome_number.py
@@ -0,0 +1,12 @@
+class PalindromeNumber:
+    def solution_1(self, x: int) -> bool:
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+
+        reverted_number: int = 0
+        while x > reverted_number:
+            reverted_number = (reverted_number * 10) + (x % 10)
+            x //= 10
+
+        reverted_number = int(reverted_number)
+        return x == reverted_number or x == reverted_number // 10
---
+++ test/general/test_palindrome_number.py
@@ -0,0 +1,20 @@
+import pytest
+
+from src.general.palindrome_number import PalindromeNumber
+
+
+class TestPalindromeNumber:
+    @pytest.mark.parametrize(
+        "input_, expected",
+        [
+            (121, True),
+            (-121, False),
+            (0, True),
+            (10, False),
+            (11, True),
+            (12321, True),
+        ],
+    )
+    def test_solution_1(self, input_, expected):
+        reverse_palindrome = PalindromeNumber()
+        assert reverse_palindrome.solution_1(input_) == expected
