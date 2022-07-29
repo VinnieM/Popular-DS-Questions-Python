62b4e303d7a0d068cde367ae501581a17f7b5a1f
Vineeth Menon <vineeth.madhu@gmail.com>
2022-07-29 03:57:32 -0400

    Minor change

--- src/general/palindrome_number.py
+++ src/general/palindrome_number.py
@@ -13,15 +13,3 @@ class PalindromeNumber:
             x //= 10
 
         return x == reverted_number or x == reverted_number // 10
-
-    def reverse(self, x):
-        negFlag = 1
-        if x < 0:
-            negFlag = -1
-            strx = str(x)[1:]
-        else:
-            strx = str(x)
-
-        x = int(strx[::-1])
-
-        return 0 if x > pow(2, 31) else x * negFlag
